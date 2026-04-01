7850c2a0c1bfa0dc90b92c99ea6f28b4e2016e36
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2026-04-01 18:58:33 +0900

    updated

---
+++ job.sh
@@ -0,0 +1,4 @@
+python load_prompts_run_video.py --input_video ./videos/ca0/ca0_00.mp4
+python load_prompts_run_video.py --input_video ./videos/ca0/ca0_01.mp4
+python load_prompts_run_video.py --input_video ./videos/ca0/ca0_02.mp4
+python load_prompts_run_video.py --input_video ./videos/ca0/ca0_03.mp4
