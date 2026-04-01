bf7141ca4bb61cf55b677a497df85210369fe5ea
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2026-04-01 19:10:43 +0900

    updated

--- job.sh
+++ job.sh
@@ -1,4 +1,4 @@
-python load_prompts_run_video.py --input_video ./videos/ca0/ca0_00.mp4
-python load_prompts_run_video.py --input_video ./videos/ca0/ca0_01.mp4
-python load_prompts_run_video.py --input_video ./videos/ca0/ca0_02.mp4
-python load_prompts_run_video.py --input_video ./videos/ca0/ca0_03.mp4
+python load_prompts_run_video.py --prompt_path ./ca0.pt --input_video ./videos/ca0/ca0_00.mp4
+python load_prompts_run_video.py --prompt_path ./ca0.pt --input_video ./videos/ca0/ca0_01.mp4
+python load_prompts_run_video.py --prompt_path ./ca0.pt --input_video ./videos/ca0/ca0_02.mp4
+python load_prompts_run_video.py --prompt_path ./ca0.pt --input_video ./videos/ca0/ca0_03.mp4
