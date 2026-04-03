4e249d64a2e7378610e73a092b4e47484fe5a853
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2026-04-03 10:00:36 +0900

    updated

--- check_generated_masks.py
+++ check_generated_masks.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 import numpy as np
 
 import tarfile
+from util.multithread_video_writer import MultithreadVideoWriter
 
 def extract_all_tar(root_dir="."):
     for file in os.listdir(root_dir):
@@ -38,15 +39,48 @@ def extract_all_tar(root_dir="."):
 def main():
     saved_dir = './saved_images/run_video/'
     tar_dirs = get_subfolders(saved_dir)
-    print(tar_dirs)
+    #print(tar_dirs)
+    for tar_dir in tar_dirs:
+        mask_name = os.path.basename(os.path.normpath(tar_dir))
+        if os.path.isdir(os.path.join(tar_dir,mask_name)):
+            print("mask already generated")
+        else:
+            process_tar_dir(tar_dir)
+        if os.path.exists(os.path.join(tar_dir,mask_name+'.mp4')):
+            print("mask video already generated")
+        else:
+            pass
 
-def main1():
-    extract_all_tar()
-    root_dir = './'
+def generate_mask_video(tar_dir):
+    mask_name = os.path.basename(os.path.normpath(tar_dir))
+    assert os.path.isdir(os.path.join(tar_dir,mask_name))
+    img_list = get_file_path_list(os.path.join(tar_dir,mask_name),'png')
+    video_writer=MultithreadVideoWriter(os.path.join(tar_dir, mask_name+'.mp4'))
+    for i in tqdm(range(len(img_list))):
+        #if i>=len(video_loader0)*0.66:
+        #    break
+        data=cv2.imread(img_list[i],cv2.IMREAD_UNCHANGED)
+        frame = data[:,:,:3]
+        mask = data[:,:,3]
+        frame[mask==0] = np.array([255,255,255],np.uint8)
+        video_writer.append(frame,False)
+    video_writer.make_video()
+    video_writer.close()
+
+
+
+
+
+
+def process_tar_dir(dir_path):
+    extract_all_tar(dir_path)
+    root_dir = dir_path
+    mask_name = os.path.basename(os.path.normpath(dir_path))
     img_dirs = get_subfolders(root_dir)
     print(img_dirs)
-    if len(img_dirs) <= 1:
-        print("Nothing to do")
+    if len(img_dirs) == 1:
+        print("Nothing to do, just rename")
+        os.rename(img_dirs[0],os.path.join(root_dir,mask_name))
         return
     img_lists = [get_file_path_list(img_dir,'png') for img_dir in img_dirs]
     for img_list in img_lists:
