b68e01c9d62bf75392a5e869d6c08680838ca80d
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2026-04-05 15:14:46 +0900

    updated

--- job.sh
+++ job.sh
@@ -12,7 +12,14 @@
 #python load_prompts_run_video.py --prompt_path ./ca2.pt --input_video ./videos/ca2/ca2_01.mp4
 #python load_prompts_run_video.py --prompt_path ./ca2.pt --input_video ./videos/ca2/ca2_02.mp4
 
-python load_prompts_run_video.py --prompt_path ./ca3.pt --input_video ./videos/ca3/ca3_00.mp4
-python load_prompts_run_video.py --prompt_path ./ca3.pt --input_video ./videos/ca3/ca3_01.mp4
-python load_prompts_run_video.py --prompt_path ./ca3.pt --input_video ./videos/ca3/ca3_02.mp4
-python load_prompts_run_video.py --prompt_path ./ca3.pt --input_video ./videos/ca3/ca3_03.mp4
+#python load_prompts_run_video.py --prompt_path ./ca3.pt --input_video ./videos/ca3/ca3_00.mp4
+#python load_prompts_run_video.py --prompt_path ./ca3.pt --input_video ./videos/ca3/ca3_01.mp4
+#python load_prompts_run_video.py --prompt_path ./ca3.pt --input_video ./videos/ca3/ca3_02.mp4
+#python load_prompts_run_video.py --prompt_path ./ca3.pt --input_video ./videos/ca3/ca3_03.mp4
+
+python load_prompts_run_video.py --prompt_path ./mx0.pt --input_video ./videos/mx0/mx0_00.mp4
+python load_prompts_run_video.py --prompt_path ./mx0.pt --input_video ./videos/mx0/mx0_01.mp4
+python load_prompts_run_video.py --prompt_path ./mx0.pt --input_video ./videos/mx0/mx0_02.mp4
+python load_prompts_run_video.py --prompt_path ./mx0.pt --input_video ./videos/mx0/mx0_03.mp4
+python load_prompts_run_video.py --prompt_path ./mx0.pt --input_video ./videos/mx0/mx0_04.mp4
+python load_prompts_run_video.py --prompt_path ./mx0.pt --input_video ./videos/mx0/mx0_05.mp4
