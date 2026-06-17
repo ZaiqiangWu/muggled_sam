d90346cf693151ef8ea62258bbe0f23acf08ec14
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2026-06-17 21:41:27 +0900

    updated

--- job.sh
+++ job.sh
@@ -24,9 +24,15 @@
 #python load_prompts_run_video.py --prompt_path ./mx0.pt --input_video ./videos/mx0/mx0_04.mp4
 #python load_prompts_run_video.py --prompt_path ./mx0.pt --input_video ./videos/mx0/mx0_05.mp4
 #python load_prompts_run_video.py --prompt_path ./mx0_02.pt --input_video ./videos/mx0/mx0_02.mp4
-python load_prompts_run_video.py --prompt_path ./pajama.pt --input_video ./videos/pajama/pajama_00.mp4
-python load_prompts_run_video.py --prompt_path ./pajama.pt --input_video ./videos/pajama/pajama_01.mp4
-python load_prompts_run_video.py --prompt_path ./gown.pt --input_video ./videos/gown/gown_00.mp4
+#python load_prompts_run_video.py --prompt_path ./pajama.pt --input_video ./videos/pajama/pajama_00.mp4
+#python load_prompts_run_video.py --prompt_path ./pajama.pt --input_video ./videos/pajama/pajama_01.mp4
+#python load_prompts_run_video.py --prompt_path ./gown.pt --input_video ./videos/gown/gown_00.mp4
+python load_prompts_run_video.py --prompt_path ./pinkdress.pt --input_video ./videos/pinkdress/pinkdress_00.mp4
+python load_prompts_run_video.py --prompt_path ./pinkdress.pt --input_video ./videos/pinkdress/pinkdress_01.mp4
+python load_prompts_run_video.py --prompt_path ./polo.pt --input_video ./videos/polo/polo_00.mp4
+python load_prompts_run_video.py --prompt_path ./polo.pt --input_video ./videos/polo/polo_01.mp4
+python load_prompts_run_video.py --prompt_path ./spacesuit.pt --input_video ./videos/spacesuit/spacesuit_00.mp4
+python load_prompts_run_video.py --prompt_path ./spacesuit.pt --input_video ./videos/spacesuit/spacesuit_01.mp4
 
 
 python check_generated_masks.py
